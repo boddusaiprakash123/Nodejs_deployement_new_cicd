126dcc41787e6d9391ea3396a1d57d522bbd3952
boddusaiprakash123 <95060708+boddusaiprakash123@users.noreply.github.com>
2024-04-19 09:19:54 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-cd /root/Nodejs_deployement_new_cicd/node_deployment
+# Define the paths
+deployment_dir="/root/Nodejs_deployement_new_cicd/node_deployment"
+new_app_path="/root/Nodejs_deployement_new_cicd/node_deployment/app.js"
+
+# Remove the existing app.js file if it exists
+if [ -f "$deployment_dir/app.js" ]; then
+  rm "$deployment_dir/app.js"
+fi
+
+# Copy the new app.js file to the deployment directory
+cp "$new_app_path" "$deployment_dir/app.js"
 
 # Install dependencies using npm
+cd "$deployment_dir"
 npm install
-
