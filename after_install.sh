dab0e8867f5103514c3ea8f508d7fd77fa80106e
boddusaiprakash123 <95060708+boddusaiprakash123@users.noreply.github.com>
2024-04-21 17:25:55 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd /root/Nodejs_deployement_new_cicd/node_deployment
 
+# Define the deployment directory
+deployment_dir="/root/Nodejs_deployement_new_cicd/node_deployment"
+
+# Create the node_modules directory inside the deployment directory
+mkdir "$deployment_dir"/node_modules
+
+# Navigate to the node_modules directory
+cd "$deployment_dir"/node_modules
+
+# Install npm dependencies
 npm install
-
